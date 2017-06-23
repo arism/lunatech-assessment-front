e3bd1d29df23b4ceb4662bab4f76112630b219ba
M.ARIS <arisoft31@gmail.com>
2017-06-23 16:47:45 +0200

    add Chart report showing the highest number of airports (with count) per country

--- src/components/ReportPage.vue
+++ src/components/ReportPage.vue
@@ -1,9 +1,22 @@
-<template>
-  <div>HI</div>
-</template>
 <script>
-export default {
+import {Bar} from 'vue-chartjs'
+import axios from 'axios'
+export default Bar.extend({
+  mounted () {
+    axios.get('/api/reports/airportsPerCountry' ).then(response => {
+      let datacollection = {
+        labels: response.data.map(c => c.country.name),
+        datasets: [
+          {
+            label: 'Airports Per Country',
+            backgroundColor: '#17caf0',
+            data: response.data.map(c => c.airportsCount)
+          }
+        ]
+      }
+       this.renderChart(datacollection, {responsive: true, maintainAspectRatio: false})
+    })
 
-}
+  }
+})
 </script>
-
