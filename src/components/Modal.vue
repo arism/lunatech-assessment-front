13bdd295ac813bb266420d277f4930e1876a09b5
M.ARIS <arisoft31@gmail.com>
2017-06-25 23:22:52 +0200

    Add getTop10RunwaysIdentifications feautre

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ui modal">
+    <i class="close icon"></i>
+    <div class="header">
+      Runways types of {{modalData.country}} airports
+    </div>
+    <div class="content">
+      <div class="description" v-for="runwaytype in modalData.runwaysType">
+        <div>
+          {{runwaytype}}
+        </div>
+      </div>
+    </div>
+    <div class="actions">
+      <div class="ui button" @click="close">OK</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['modalData'],
+  methods: {
+    close(){
+      $('.ui.modal').modal("hide")
+    }
+  }
+}
+</script>
+
--- src/components/ReportPage.vue
+++ src/components/ReportPage.vue
@@ -1,49 +1,78 @@
 <template>
   <div>
-    <Toggle @toggle="refreshChart"></Toggle>
-    <div class="chart-container" style="position: relative; width:850px;">
-        <Chart :chartData="chartData" @chartSelect="onChartSelect"></Chart>
-        <div class="ui yellow message">Please click on the chart Bar to show more details</div>
-    </div>
+    <table>
+      <tr>
+        <td>
+          <Toggle @toggle="refreshChart"></Toggle>
+          <div class="chart-container" style="position: relative; width:850px;">
+            <Chart :chartData="chartData" @chartSelect="onChartSelect"></Chart>
+            <div class="ui yellow message">Please click on the chart Bar to show more details</div>
+          </div>
+        </td>
+        <td  class="ui">
+          <h4>Top 10 most common runway identifications</h4>
+          <ul v-for="ident in top10RunwaysIdent">
+            <li>{{ident}}</li>
+          </ul>
+        </td>
+      </tr>
+    </table>
+    <Modal :modalData="modalData"></Modal>
   </div>
 </template>
 <script>
 import axios from 'axios'
 import Chart from './Chart'
 import Toggle from './Toggle'
+import Modal from './Modal'
 export default {
   components: {
     Chart,
-    Toggle
+    Toggle,
+    Modal
   },
   data() {
     return {
-      chartData: {}
+      chartData: {},
+      modalData: {},
+      top10RunwaysIdent: []
     }
   },
-  mounted(){
+  mounted() {
     this.refreshChart("1")
+    this.showTop10RunwaysIdent()
   },
   methods: {
     refreshChart(desc) {
-      axios.get('/api/reports/airportsPerCountry?desc='+desc).then(response => {
-      this.chartData = {
-        labels: response.data.map(c => c.country.name),
-        datasets: [
-          {
-            label: 'Airports Per Country',
-            backgroundColor: '#17caf0',
-            data: response.data.map(c => c.airportsCount)
-          }
-        ],
-        runwaysType: response.data.map(c => c.runwaysType)
-      }
-    })
+      axios.get('/api/reports/airportsPerCountry?desc=' + desc).then(response => {
+        this.chartData = {
+          labels: response.data.map(c => c.country.name),
+          datasets: [
+            {
+              label: 'Airports Per Country',
+              backgroundColor: '#17caf0',
+              data: response.data.map(c => c.airportsCount)
+            }
+          ],
+          runwaysType: response.data.map(c => c.runwaysType)
+        }
+      })
     },
-    onChartSelect(idx){
-      if(this.chartData.runwaysType && this.chartData.runwaysType.length ){
-        console.log(this.chartData.runwaysType[idx])
+    onChartSelect(idx) {
+      if (this.chartData.runwaysType && this.chartData.runwaysType.length) {
+        this.modalData = {
+          country: this.chartData.labels[idx],
+          runwaysType: this.chartData.runwaysType[idx]
+        }
+        setTimeout(() => {
+          $('.ui.modal').modal("show")
+        }, 100)
       }
+    },
+    showTop10RunwaysIdent(){
+      axios.get('/api/reports/top10RunwaysIdent').then(response => {
+        this.top10RunwaysIdent = response.data
+      })
     }
   }
 }
